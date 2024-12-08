01fbe4aea3192dc9af420fe007247b963468bbcb
hawmie <nomenaloic.mail@gmail.com>
2024-12-08 07:13:29 +0300

    test online

--- backend/src/config/GDrive.js
+++ backend/src/config/GDrive.js
@@ -1,6 +1,9 @@
 import { google } from 'googleapis'
 import { fileURLToPath } from 'url'
 import path from 'path'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 
 const __filename = fileURLToPath(import.meta.url)
@@ -11,7 +14,10 @@ const FOLDER_ID = "10hS0NMimvNhzWNeYf7OJ_AXq5bOPrnAq"
 const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
 
 const auth = new google.auth.GoogleAuth({
-    keyFile: path.join(__dirname,'../../credentials.json'),
+    credentials:{
+        private_key:process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    },
     scopes:SCOPES,
 })
 
